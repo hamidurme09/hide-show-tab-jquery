9d37b154c4f4f912e321aaf5c821536f354837cf
hamidurme09 <hamidur.me09@gmail.com>
2020-03-30 14:30:24 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  console.log("ready");
+  navIcon();
+  userNavIcon();
+  contactNavIcon();
+
+  function navIcon() {
+    // by default hide all content div elements except first element
+    $(".mch-content .mch-content__item").hide();
+    $(".mch-content .mch-content__item:first-child").show();
+
+    $(".mch-nav ul li").click(function() {
+      $(".mch-nav ul li").removeClass("active");
+      $(this).addClass("active");
+
+      var current_tab_value = $(this).attr("data-list");
+      $(".mch-content .mch-content__item").hide();
+      $("." + current_tab_value).show();
+    });
+  }
+
+  function userNavIcon() {
+    $(
+      "#mch-content-3 .mch-content-3__content .mch-content-3__content__item"
+    ).hide();
+    $(
+      "#mch-content-3 .mch-content-3__content .mch-content-3__content__item:first-child"
+    ).show();
+
+    $("#mch-content-3 ul li").click(function() {
+      $("#mch-content-3 ul li").removeClass("active");
+      $(this).addClass("active");
+
+      var current_tab_value = $(this).attr("data-list");
+      console.log(current_tab_value);
+      $("#mch-content-3 .mch-content-3__content__item").hide();
+      $("." + current_tab_value).show();
+    });
+  }
+
+  function contactNavIcon() {
+    $(
+      "#mch-content-4 .mch-content-4__content .mch-content-4__content__item"
+    ).hide();
+    $(
+      "#mch-content-4 .mch-content-4__content .mch-content-4__content__item:first-child"
+    ).show();
+
+    $("#mch-content-4 ul li").click(function() {
+      $("#mch-content-4 ul li").removeClass("active");
+      $(this).addClass("active");
+
+      var current_tab_value = $(this).attr("data-list");
+      console.log(current_tab_value);
+      $("#mch-content-4 .mch-content-4__content__item").hide();
+      $("." + current_tab_value).show();
+    });
+  }
+});
